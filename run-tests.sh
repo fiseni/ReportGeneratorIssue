4233e33e35c8d260bf7e02e6160b05f9905edd4c
Fati Iseni <fati.iseni@pozitrongroup.com>
2025-06-10 22:23:00 +0200

    Use opencover format

--- run-tests.sh
+++ run-tests.sh
@@ -3,10 +3,10 @@
 find . -type d -name TestResults -exec rm -rf {} \; > /dev/null 2>&1
 
 dotnet build --configuration Release
-dotnet test --configuration Release --no-build --no-restore --collect:"XPlat Code Coverage"
+dotnet test --configuration Release --no-build --no-restore --collect:"XPlat Code Coverage;Format=opencover"
 
 reportgenerator \
-    -reports:tests/**/coverage.cobertura.xml \
+    -reports:tests/**/coverage.opencover.xml \
     -targetdir:TestResults \
     -reporttypes:"Html;Badges;MarkdownSummaryGithub" \
     -assemblyfilters:-*Tests*
